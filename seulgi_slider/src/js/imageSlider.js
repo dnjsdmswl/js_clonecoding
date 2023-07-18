404257de6aa0cb2bba6186667696be14b71c4978
은지 <yeochinlebelomgeole@gmail.com>
2023-07-18 16:22:51 +0900

    Height_slider
    세로 이미지 슬라이더

---
+++ seulgi_slider/src/js/imageSlider.js
@@ -0,0 +1,147 @@
+export default class ImageSlider {
+  #currentPosition = 0;
+
+  #slideNumber = 0;
+
+  #slideHeight = 0;
+
+  #intervalId;
+
+  #autoPlay = true;
+
+  sliderWrapEl;
+
+  sliderListEl;
+
+  nextBtnEl;
+
+  previousBtnEl;
+
+  indicatorWrapEl;
+
+  controlWrapEl;
+
+  constructor() {
+    this.assignElement();
+    this.initSliderNumber();
+    this.initSlideHeight();
+    this.initSliderListHeight();
+    this.addEvent();
+    this.createindicator();
+    this.setindicator();
+    this.initAutoplay();
+  }
+
+  assignElement() {
+    this.sliderWrapEl = document.getElementById('slider-wrap');
+    this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
+    this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
+    this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+    this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
+    this.controlWrapEl = this.sliderWrapEl.querySelector('#control-wrap');
+  }
+
+  addEvent() {
+    this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
+    this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+    this.indicatorWrapEl.addEventListener(
+      'click',
+      this.onClickIndicator.bind(this),
+    );
+    this.controlWrapEl.addEventListener('click', this.togglePlay.bind(this));
+  }
+
+  initSliderNumber() {
+    this.#slideNumber = this.sliderListEl.querySelectorAll('li').length;
+  }
+
+  initSlideHeight() {
+    this.#slideHeight = this.sliderListEl.clientHeight;
+  }
+
+  initSliderListHeight() {
+    this.sliderListEl.style.Height = `${
+      this.#slideNumber * this.#slideHeight
+    }px`;
+  }
+
+  initAutoplay() {
+    this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+  }
+
+  moveToRight() {
+    this.#currentPosition += 1;
+    if (this.#currentPosition === this.#slideNumber) {
+      this.#currentPosition = 0;
+    }
+    this.sliderListEl.style.top = `-${
+      this.#currentPosition * this.#slideHeight
+    }px`;
+    this.setindicator();
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.initAutoplay();
+    }
+  }
+
+  moveToLeft() {
+    this.#currentPosition -= 1;
+    if (this.#currentPosition === -1) {
+      this.#currentPosition = this.#slideNumber - 1;
+    }
+    this.sliderListEl.style.top = `-${
+      this.#currentPosition * this.#slideHeight
+    }px`;
+    this.setindicator();
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.initAutoplay();
+    }
+  }
+
+  createindicator() {
+    const docFragment = document.createDocumentFragment();
+    for (let i = 0; i < this.#slideNumber; i += 1) {
+      const li = document.createElement('li');
+      li.dataset.index = i; // 동그라미에 인덱스 넣어주기이
+      docFragment.appendChild(li);
+    }
+    this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+  }
+
+  setindicator() {
+    this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active');
+    this.indicatorWrapEl
+      .querySelector(`ul li:nth-child(${this.#currentPosition + 1})`)
+      .classList.add('active');
+  }
+
+  onClickIndicator(event) {
+    const indexPosition = parseInt(event.target.dataset.index, 10);
+    if (Number.isInteger(indexPosition)) {
+      this.#currentPosition = indexPosition;
+      this.sliderListEl.style.top = `-${
+        this.#currentPosition * this.#slideHeight
+      }px`;
+    }
+    this.setindicator();
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.initAutoplay();
+    }
+  }
+
+  togglePlay(event) {
+    if (event.target.dataset.status === 'play') {
+      this.#autoPlay = true;
+      this.controlWrapEl.classList.add('play');
+      this.controlWrapEl.classList.remove('pause');
+      this.initAutoplay();
+    } else if (event.target.dataset.status === 'pause') {
+      this.#autoPlay = false;
+      this.controlWrapEl.classList.add('pause');
+      this.controlWrapEl.classList.remove('play');
+      clearInterval(this.#intervalId);
+    }
+  }
+}
